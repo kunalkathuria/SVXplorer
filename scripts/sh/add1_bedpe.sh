47e0ce8eae575cb9f89010b341b7aa2c22f0af20
Aakrosh Ratan <ratan@bx.psu.edu>
2017-12-18 16:50:06 -0500

    Deleted files

--- scripts/sh/add1_bedpe.sh
+++
@@ -1,8 +0,0 @@
-file=$1
-cat $file | cut -f1 > c1
-cat $file | cut -f2 > c2
-cat $file | cut -f3 > c6
-cat $file | awk '{print $2+1}' > c3
-cat $file | awk '{print $3-1}' > c5
-paste c1 c2 c3 c1 c5 c6 > $file.6col.bedpe
-rm c1 c2 c3 c5 c6
