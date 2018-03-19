916ec5caeff0b6d2e979880c37e0170ff5c8403f
Kunal Kathuria <kk7t@ap03.cphg.virginia.edu>
2018-03-18 23:23:41 -0400

    Added DN_INS to check for size threshold

--- src/covPUFilter.py
+++ src/covPUFilter.py
@@ -203,7 +203,8 @@ def covPUFilter(workDir, avFile, vmFile, ufFile, statFile, bamFile,
 
             ## skip SV if size thresholds not met
             if svtype.startswith("TD") or svtype.startswith("INV") or svtype.startswith("DEL") or\
-                (svtype.startswith("INS_half") and lineAV_split[2] == lineAV_split[5]):
+                (svtype.startswith("INS_half") and lineAV_split[2] == lineAV_split[5]) or\
+                svtype == "DN_INS":
                 if int(lineAV_split[7])-int(lineAV_split[3]) < UNIV_VAR_THRESH:
                     continue
 
