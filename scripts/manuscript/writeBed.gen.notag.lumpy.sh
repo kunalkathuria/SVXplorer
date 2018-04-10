51ff3101def177192807c484cf0cb44bd05e5551
Kunal Kathuria <kk7t@ap03.cphg.virginia.edu>
2018-04-10 16:59:14 -0400

    script modification

--- scripts/manuscript/writeBed.gen.notag.lumpy.sh
+++ scripts/manuscript/writeBed.gen.notag.lumpy.sh
@@ -8,7 +8,7 @@ python ../vcftoBedpe.py $DIR/DEL.$3.vcf 2
 cat $1 | grep -v GL00 | grep -v NC_ | grep -v hs3 | grep -v -w MT | grep -v BND | grep -v "0\/0" | grep -v "\.\/\." | grep DUP > $DIR/DUP.$3.vcf
 python ../vcftoBedpe.py $DIR/DUP.$3.vcf 2
 
-cat $1 | grep -v GL00 | grep -v NC_ | grep -v hs3 | grep -v -w MT | grep -v BND | grep INV | grep -v ISINV > $DIR/INV.$3.vcf
+cat $1 | grep -v "\.\/\." | grep -v "0\/0" | grep -v GL00 | grep -v NC_ | grep -v hs3 | grep -v -w MT | grep -v BND | grep INV | grep -v ISINV > $DIR/INV.$3.vcf
 python ../vcftoBedpe.py $DIR/INV.$3.vcf 2
 
 #cat $1 | awk '$7 == "PASS"' | grep BND > $DIR/BND.$3.vcf
